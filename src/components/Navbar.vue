24befee83f2e4632f46ca0e06e5ee4d02a7a5605
Pedro Fernandes <pfernandes180103@gmail.com>
2023-09-15 17:13:58 -0300

    Modelando o projeto, rotas e comps repetitivos

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template >
+    <header>
+            <nav>
+                <RouterLink class="links" to="/"><img src="" alt="logo de hambueger" id="logo"></RouterLink>
+                <RouterLink class="links" to="/">Home</RouterLink>
+                <RouterLink class="links" to="/pedidos">Pedidos</RouterLink>
+            </nav>
+    </header>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+    name: 'Navbar',
+}
+</script>
+<style scoped>
+    nav {
+        display: flex;
+        justify-content: flex-start;
+    }
+
+    .links {
+        margin: 0 10px;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home">
+    <h1>Home</h1>
   </main>
 </template>
+
+
+<script >
+
+</script>
