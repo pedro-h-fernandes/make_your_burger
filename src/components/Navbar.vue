97ea12c513cd13f4f91f13815cde39630b8e88f2
Pedro Fernandes <pfernandes180103@gmail.com>
2023-10-22 15:38:44 -0300

    navbar com bootstrap

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,27 @@
 <template >
     <header>
-        <nav>
-            <RouterLink id="logo-url" class="links" to="/"><img :src="logo" :alt="alt" id="logo"></RouterLink>
-            <RouterLink class="links" to="/">Home</RouterLink>
-            <RouterLink class="links" to="/pedidos">Pedidos</RouterLink>
+        <nav class="navbar fixed-top navbar-expand-sm py-4 px-5 d-flex justify-content-md-between" data-bs-theme="dark">
+            <div class="container-fluid d-flex justify-content-space-between">
+                <RouterLink id="logo-url" class="links" to="/"><img :src="logo" :alt="alt" id="logo"></RouterLink>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item">
+                            <RouterLink class="links" to="/">Home</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="links" to="/pedidos">Pedidos</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </header>
 </template>
@@ -18,29 +36,20 @@ export default {
 }
 </script>
 <style scoped>
-nav {
-    background-color: #222;
-    border-bottom: 4px solid #111;
-    padding: 15px 50px;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-}
-
-#logo-url {
-    margin: auto;
-    margin-left: 0;
-}
 
 #logo {
-    max-width: 40px;
-    max-height: 40p;
+    max-width: 45px;
+    max-height: 45px;
+}
+
+nav{
+    background-color: #222;
 }
 
 .links {
     margin: 12px;
     text-decoration: none;
-    font-size: 1.8rem;
+    font-size: 1.9rem;
     color: #fcba03;
     transition: .5s;
 }
